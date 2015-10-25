e609b46f8de1b11aad119707b904f7114891d6f7
unovor <think@entelechyi.com>
2015-10-25 03:07:24 -0700

    Update simple-todos-react.jsx

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -1,7 +1,9 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code is executed on the client only
  // Define a collection to hold our tasks
-Tasks = new Mongo.Collection("tasks");
+
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
     React.render(<App />, document.getElementById("render-target"));
