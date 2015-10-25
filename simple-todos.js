5671343c847890c0bbb130adec7baa1039294f50
unovor <think@entelechyi.com>
2015-10-25 02:53:06 -0700

    Update simple-todos.js

--- simple-todos.js
+++ simple-todos.js
@@ -1,40 +1,8 @@
-Tasks = new Mongo.Collection("tasks");
- 
 if (Meteor.isClient) {
-  // This code only runs on the client
-  Template.body.helpers({
-    tasks: function () {
-            return Tasks.find({}, {sort: {createdAt: -1}});
-    }
-  });
+  // This code is executed on the client only
  
-  Template.body.events({
-    "submit .new-task": function (event) {
-      // Prevent default browser form submit
-      event.preventDefault();
- 
-      // Get value from form element
-      var text = event.target.text.value;
- 
-      // Insert a task into the collection
-      Tasks.insert({
-        text: text,
-        createdAt: new Date() // current time
-      });
- 
-      // Clear form
-      event.target.text.value = "";
-    }
-  });
-  Template.task.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Tasks.update(this._id, {
-        $set: {checked: ! this.checked}
-      });
-    },
-    "click .delete": function () {
-      Tasks.remove(this._id);
-    }
+  Meteor.startup(function () {
+    // Use Meteor.startup to render the component after the page is ready
+    React.render(<App />, document.getElementById("render-target"));
   });
 }
