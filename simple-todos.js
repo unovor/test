1bf81ad2f3f649250ace472a2710551ec40bc145
unovor <think@entelechyi.com>
2015-10-24 23:34:41 -0700

    Update simple-todos.js

--- simple-todos.js
+++ simple-todos.js
@@ -5,6 +5,25 @@ if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function () {
       return Tasks.find({});
+      
+        Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+ 
+      // Get value from form element
+      var text = event.target.text.value;
+ 
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      event.target.text.value = "";
+    }
+  });
     }
   });
 }
