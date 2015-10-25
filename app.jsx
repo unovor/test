255f1841652770f660cc34d67cd7a4f372d042e1
unovor <think@entelechyi.com>
2015-10-25 03:18:59 -0700

    Update app.jsx

--- app.jsx
+++ app.jsx
@@ -1,13 +1,20 @@
 App = React.createClass({
-  getTasks() {
-    return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
-    ];
+    // This mixin makes the getMeteorData method work
+  mixins: [ReactMeteorData],
+ 
+  // Loads items from the Tasks collection and puts them on this.data.tasks
+  getMeteorData() {
+    return {
+      tasks: Tasks.find({}).fetch()
+    }
+    
   },
+  
+
  
   renderTasks() {
+        // Get tasks from this.data.tasks
+
     return this.getTasks().map((task) => {
       return <Task key={task._id} task={task} />;
     });
